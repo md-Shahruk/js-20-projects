c51c8a7e271b61f3f0b0ad4683394fa52759e0a6
Shahruk <mdshahruk2020@gmail.com>
2026-03-28 23:42:26 +0600

    design calculator app using html css

---
+++ Bro_code_js/index_1.js
@@ -0,0 +1,31 @@
+
+// how to accept user input
+/* 
+callback = a function that is passed as an argument to another function
+use:
+   1. Reading a file
+   2. Network requests
+   3. Interecting with database
+*/
+// callback with foreach: forEach by defult element, index, array provided
+
+// let numbers = [1,2,3,4];
+
+// numbers.forEach(square);
+// numbers.forEach(display);
+
+// function double(element, index, array){
+//     array[index] = element * 2;
+// }
+// function square(element, index, array){
+//     array[index] = Math.pow(element, 2);
+// }
+
+// function display(element){
+//     console.log(element);
+    
+// } 
+
+/*
+.map() 
+*/
