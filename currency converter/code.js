3fdd2d16c8c40673283c1c52a42dca2a15a6ee01
Shahruk <mdshahruk2020@gmail.com>
2026-03-29 15:57:44 +0600

    desing currency converter using html css

---
+++ currency converter/code.js
@@ -0,0 +1,7 @@
+const firstCurrencySelect = document.getElementById('first-currency');
+const secondCurrencySelect = document.getElementById('second-currency');
+
+const firstCurrencyInput =  document.getElementById('c-first');
+const secondCurrencyInput = document.getElementById('c-second');
+
+const exchangeRate = document.getElementById('exchange-rate');
